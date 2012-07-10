8dffee1d42d3735f509e1f5215a636fd44754516
DarkGuardsman <rseifert.phone@gmail.com>
2012-07-10 06:38:59 -0300

    Added doc to code

--- minecraft/net/minecraft/src/eui/pipes/api/ILiquidProducer.java
+++ minecraft/net/minecraft/src/eui/pipes/api/ILiquidProducer.java
@@ -10,12 +10,19 @@ public interface ILiquidProducer
 	/**
 	 * onProduceLiquid  
 	 * block.
-	 * @param type - the type of liquid or gas
-	 * @param maxvol - The maximum vol 
+	 * @param type - the type of liquid 
+	 * @param maxvol - The maximum vol or requested volume
 	 * @param side - The side 
-	 * @return vol - Return a vol of liquid type
+	 * @return vol - Return a vol of liquid type that is produced
 	 */
 	public int onProduceLiquid(int type, int maxVol, int side);
-	
+	/**
+	 * canProduceLiquid  
+	 * block.
+	 * @param type - the type of liquid 
+	 * @param side - The side 
+	 * @return boolean - True if can, false if can't produce liquid of type or on that side
+	 * Also used for connection rules of pipes'
+	 */
 	public boolean canProduceLiquid(int type, byte side);
 }
