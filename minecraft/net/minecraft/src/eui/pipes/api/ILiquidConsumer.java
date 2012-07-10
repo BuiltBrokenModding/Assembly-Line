6a704fbc8b8e7c4497987b7e33a51a464f2436a4
DarkGuardsman <rseifert.phone@gmail.com>
2012-07-10 06:36:49 -0300

    Add docs to code

--- minecraft/net/minecraft/src/eui/pipes/api/ILiquidConsumer.java
+++ minecraft/net/minecraft/src/eui/pipes/api/ILiquidConsumer.java
@@ -7,6 +7,7 @@ public interface ILiquidConsumer
 	 * onRecieveLiquid
 	 * @param vol - The amount this block received.
 	 * @param side - The side of the block in which the liquid came from.
+	 * @parm type - The type of liquid being received
 	 * @return vol - The amount liquid that can't be recieved
 	 */
 	public int onReceiveLiquid(int type, int vol, byte side);
@@ -14,6 +15,7 @@ public interface ILiquidConsumer
 	/**
 	 * You can use this to check if a pipe can connect to this liquid consumer to properly render the graphics
 	 * @param side - The side in which the electricity is coming from.
+	 * @parm type - The type of liquid 
 	 * @return Returns true or false if this consumer can receive electricity at this given tick or moment.
 	 */
 	public boolean canRecieveLiquid(int type, byte side);
