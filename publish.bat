5938dd30f1f50c2a5576d8b644fef3a1cd111e45
Calclavia <henry@calclavia.com>
2012-11-03 17:19:42 +0800

    Updating build script to upload APIs to server

--- publish.bat
+++ publish.bat
@@ -9,7 +9,7 @@ set /a BUILD_NUMBER=%CurrentBuild%+1
 echo %BUILD_NUMBER% >buildnumber.txt
 
 set FILE_NAME=AssemblyLine_v%MODVERSION%.%BUILD_NUMBER%.jar
-::set API_NAME=AssemblyLine_v%MODVERSION%.%BUILD_NUMBER%_api.zip
+set API_NAME=AssemblyLine_v%MODVERSION%.%BUILD_NUMBER%_api.zip
 set BACKUP_NAME=AssemblyLine_v%MODVERSION%.%BUILD_NUMBER%_backup.zip
 
 echo Starting to build %FILE_NAME%
@@ -31,11 +31,11 @@ cd resources\
 cd ..\
 cd src\
 "..\..\7za.exe" a "..\builds\%BACKUP_NAME%" "*\assemblyline\" -pdarkguardsman
-::"..\..\7za.exe" a "..\builds\%API_NAME%" "*\atomicscience\api\"
+"..\..\7za.exe" a "..\builds\%API_NAME%" "*\assemblyline\api\"
 cd ..\
 
 ::UPDATE INFO FILE
-echo %PROMOTION% %FILE_NAME%>>info.txt
+echo %PROMOTION% %FILE_NAME% %API_NAME%>>info.txt
 
 ::GENERATE FTP Script
 echo open www.calclavia.com>ftpscript.txt
@@ -43,7 +43,7 @@ echo al@calclavia.com>>ftpscript.txt
 echo VkE4laBa84R9>>ftpscript.txt
 echo binary>>ftpscript.txt
 echo put "builds\%FILE_NAME%">>ftpscript.txt
-::echo put "builds\%API_NAME%">>ftpscript.txt
+echo put "builds\%API_NAME%">>ftpscript.txt
 echo put info.txt>>ftpscript.txt
 echo quit>>ftpscript.txt
 ftp.exe -s:ftpscript.txt
