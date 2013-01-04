be43631eae3b90bf90f181a158cc203944c06905
Henry Mao <admin@calclavia.com>
2013-01-04 12:26:59 +0800

    Version Bump

--- src/minecraft/assemblyline/common/AssemblyLine.java
+++ src/minecraft/assemblyline/common/AssemblyLine.java
@@ -47,7 +47,7 @@ public class AssemblyLine
 
 	public static final String NAME = "Assembly Line";
 
-	public static final String VERSION = "0.2.1";
+	public static final String VERSION = "0.2.2";
 
 	public static final String CHANNEL = "AssemblyLine";
 
