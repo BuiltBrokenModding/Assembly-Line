d62c13e675721e79843df49c26679fd5bcd052ac
Rseifert <rseifert.phone@gmail.com>
2013-01-09 23:11:17 -0500

    Correct Slanted Belt Render Issue
    Some one on the forum noticed this issue with Slanted Belts not
    rendering connected. I didn't know this was a feature that was usable or
    i would have caught it sooner. Anyways its mostly fixed but needs a
    texture which i'll try to create.
    
    Also who every made the conveyor belt textures animated like that nice
    job.

--- src/minecraft/assemblyline/client/render/RenderConveyorBelt.java
+++ src/minecraft/assemblyline/client/render/RenderConveyorBelt.java
@@ -5,6 +5,7 @@ import net.minecraft.tileentity.TileEntity;
 
 import org.lwjgl.opengl.GL11;
 
+import assemblyline.client.model.ModelAngledBelt;
 import assemblyline.client.model.ModelConveyorBelt;
 import assemblyline.common.AssemblyLine;
 import assemblyline.common.machine.belt.TileEntityConveyorBelt;
@@ -15,82 +16,70 @@ import cpw.mods.fml.relauncher.SideOnly;
 @SideOnly(Side.CLIENT)
 public class RenderConveyorBelt extends TileEntitySpecialRenderer
 {
-	private ModelConveyorBelt model = new ModelConveyorBelt();
+    private ModelConveyorBelt model = new ModelConveyorBelt();
+    private ModelAngledBelt model2 = new ModelAngledBelt();
 
-	private void renderAModelAt(TileEntityConveyorBelt tileEntity, double x, double y, double z, float f)
-	{
-		boolean mid = tileEntity.getIsMiddleBelt();
-		SlantType slantType = tileEntity.getSlant();
-		int face = tileEntity.getDirection().ordinal();
+    private void renderAModelAt(TileEntityConveyorBelt tileEntity, double x, double y, double z, float f)
+    {
+        boolean mid = tileEntity.getIsMiddleBelt();
+        SlantType slantType = tileEntity.getSlant();
+        int face = tileEntity.getDirection().ordinal();
 
-		GL11.glPushMatrix();
-		GL11.glTranslatef((float) x + 0.5F, (float) y + 1.5F, (float) z + 0.5F);
-		GL11.glRotatef(180f, 0f, 0f, 1f);
+        GL11.glPushMatrix();
+        GL11.glTranslatef((float) x + 0.5F, (float) y + 1.5F, (float) z + 0.5F);
+        GL11.glRotatef(180f, 0f, 0f, 1f);
 
-		int frame = tileEntity.getAnimationFrame();
+        int frame = tileEntity.getAnimationFrame();
 
-		this.bindTextureByName(AssemblyLine.TEXTURE_PATH + "belt/frame" + frame + ".png");
+        switch (face)
+        {
+            case 2:
+                GL11.glRotatef(0f, 0f, 1f, 0f);
+                break;
+            case 3:
+                GL11.glRotatef(180f, 0f, 1f, 0f);
+                break;
+            case 4:
+                GL11.glRotatef(-90f, 0f, 1f, 0f);
+                break;
+            case 5:
+                GL11.glRotatef(90f, 0f, 1f, 0f);
+                break;
+        }
 
-		switch (face)
-		{
-			case 2:
-				GL11.glRotatef(0f, 0f, 1f, 0f);
-				break;
-			case 3:
-				GL11.glRotatef(180f, 0f, 1f, 0f);
-				break;
-			case 4:
-				GL11.glRotatef(-90f, 0f, 1f, 0f);
-				break;
-			case 5:
-				GL11.glRotatef(90f, 0f, 1f, 0f);
-				break;
-		}
+        if (slantType != null && slantType != SlantType.NONE)
+        {
+            this.bindTextureByName(AssemblyLine.TEXTURE_PATH + "Grey64.png");
+            if (slantType == SlantType.UP)
+            {
+                GL11.glRotatef(180f, 0f, 1f, 0f);
+                model2.render(0.0625F);
+            }
+            else if (slantType == SlantType.DOWN)
+            {
 
-		if (slantType != null && slantType != SlantType.NONE)
-		{
-			GL11.glTranslatef(0, -0.3f, 0);
-			GL11.glScalef(1f, 1.3f, 1.4f);
+                
+                model2.render(0.0625F);
 
-			if (slantType == SlantType.UP)
-			{
-				if (face == 5 || face == 4)
-				{
-					GL11.glTranslatef(0f, 0f, 1f);
-					GL11.glRotatef(-45f, 1f, 0f, 0f);
-				}
-				else if (face == 2 || face == 3)
-				{
-					GL11.glTranslatef(0f, 0f, 1f);
-					GL11.glRotatef(-45f, 1f, 0f, 0f);
-				}
-			}
-			else if (slantType == SlantType.DOWN)
-			{
-				if (face == 5 || face == 4)
-				{
-					GL11.glTranslatef(0f, 0f, -1f);
-					GL11.glRotatef(45f, 1f, 0f, 0f);
-				}
-				else if (face == 2 || face == 3)
-				{
-					GL11.glTranslatef(0f, 0f, -1f);
-					GL11.glRotatef(45f, 1f, 0f, 0f);
-				}
-			}
-		}
+            }
+        }
+        else
+        {
+            this.bindTextureByName(AssemblyLine.TEXTURE_PATH + "belt/frame" + frame + ".png");
+            model.render(0.0625F, (float) Math.toRadians(tileEntity.wheelRotation), tileEntity.getIsLastBelt(), tileEntity.getIsFirstBelt(), false);
 
-		int ent = tileEntity.worldObj.getBlockId(tileEntity.xCoord, tileEntity.yCoord, tileEntity.zCoord);
-		model.render(0.0625F, (float) Math.toRadians(tileEntity.wheelRotation), tileEntity.getIsLastBelt(), tileEntity.getIsFirstBelt(), false);
+        }
 
-		GL11.glPopMatrix();
+        int ent = tileEntity.worldObj.getBlockId(tileEntity.xCoord, tileEntity.yCoord, tileEntity.zCoord);
 
-	}
+        GL11.glPopMatrix();
 
-	@Override
-	public void renderTileEntityAt(TileEntity tileEntity, double var2, double var4, double var6, float var8)
-	{
-		this.renderAModelAt((TileEntityConveyorBelt) tileEntity, var2, var4, var6, var8);
-	}
+    }
+
+    @Override
+    public void renderTileEntityAt(TileEntity tileEntity, double var2, double var4, double var6, float var8)
+    {
+        this.renderAModelAt((TileEntityConveyorBelt) tileEntity, var2, var4, var6, var8);
+    }
 
 }
