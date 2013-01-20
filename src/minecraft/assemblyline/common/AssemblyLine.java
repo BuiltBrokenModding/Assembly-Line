d043109284218c436dbc3a83d0f0309fe0428317
Brian Ricketts <briman94@gmail.com>
2013-01-19 23:30:13 -0600

    Added IUseable and apparently updated some build things.

--- src/minecraft/assemblyline/common/AssemblyLine.java
+++ src/minecraft/assemblyline/common/AssemblyLine.java
@@ -51,7 +51,7 @@ public class AssemblyLine
 
 	public static final String NAME = "Assembly Line";
 
-	public static final String VERSION = "0.2.3";
+	public static final String VERSION = "0.2.4";
 
 	public static final String CHANNEL = "AssemblyLine";
 
---
+++ src/minecraft/assemblyline/common/machine/armbot/IUseable.java
@@ -0,0 +1,24 @@
+package assemblyline.common.machine.armbot;
+
+import net.minecraft.entity.Entity;
+
+/**
+ * The IUseable inteface is used by the ArmBot so that it may interact with Tile Entities.
+ * onUse will be called on the block an ArmBot is touching whenver the USE command is run
+ * on it.
+ * @author Briman0094
+ *
+ */
+public interface IUseable
+{
+	
+	/**
+	 * Called when the ArmBot command "USE" is run. This is called on any IUseable the
+	 * ArmBot is touching.
+	 * @param tileEntity the TileEntityArmbot that is using this IUseable
+	 * @param heldEntity the Entity being held by the ArmBot, or null if there is none
+	 * @return whether or not the "use" did anything
+	 */
+	public boolean onUse(TileEntityArmbot tileEntity, Entity heldEntity);
+	
+}
