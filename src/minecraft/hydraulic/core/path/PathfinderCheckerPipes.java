7488a448f0205e015a6a3765ad0858aa9c9ef5b6
Rseifert <rseifert.phone@gmail.com>
2013-03-30 00:21:33 -0400

    Made my own path finder checker
    It worked without this but just too be safe i copied and changed the one
    i was using from UE.

---
+++ src/minecraft/hydraulic/core/path/PathfinderCheckerPipes.java
@@ -0,0 +1,48 @@
+package hydraulic.core.path;
+
+import hydraulic.api.IPipeConnection;
+
+import java.util.Arrays;
+
+import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.common.ForgeDirection;
+import universalelectricity.core.block.IConductor;
+import universalelectricity.core.block.IConnectionProvider;
+import universalelectricity.core.path.Pathfinder;
+
+/**
+ * Check if a conductor connects with another.
+ */
+public class PathfinderCheckerPipes extends Pathfinder
+{
+	public PathfinderCheckerPipes(final IConnectionProvider targetConnector, final IConnectionProvider... ignoreConnector)
+	{
+		super(new IPathCallBack()
+		{
+			@Override
+			public boolean isValidNode(Pathfinder finder, ForgeDirection direction, IConnectionProvider provider, IConnectionProvider connectedBlock)
+			{
+				if (connectedBlock instanceof IPipeConnection && !Arrays.asList(ignoreConnector).contains(connectedBlock))
+				{
+					if (((IPipeConnection) connectedBlock).canConnect((TileEntity) connectedBlock, direction.getOpposite()))
+					{
+						return true;
+					}
+				}
+				return false;
+			}
+
+			@Override
+			public boolean onSearch(Pathfinder finder, IConnectionProvider provider)
+			{
+				if (provider == targetConnector)
+				{
+					finder.results.add(provider);
+					return true;
+				}
+
+				return false;
+			}
+		});
+	}
+}
