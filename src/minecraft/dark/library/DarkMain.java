3a7b0e79518eb62127e1dd8a46224741cdea98df
Robert Seifert <rseifert.phone@gmail.com>
2013-04-20 08:24:24 -0400

    added assets

---
+++ src/minecraft/dark/library/DarkMain.java
@@ -0,0 +1,15 @@
+package dark.library;
+
+public class DarkMain
+{
+	/* RESOURCE FILE PATHS */
+	public static final String RESOURCE_PATH = "/mods/dark/";
+	public static final String TEXTURE_DIRECTORY = RESOURCE_PATH + "textures/";
+	public static final String GUI_DIRECTORY = TEXTURE_DIRECTORY + "gui/";
+	public static final String BLOCK_TEXTURE_DIRECTORY = TEXTURE_DIRECTORY + "blocks/";
+	public static final String ITEM_TEXTURE_DIRECTORY = TEXTURE_DIRECTORY + "items/";
+	public static final String MODEL_TEXTURE_DIRECTORY = TEXTURE_DIRECTORY + "models/";
+	public static final String TEXTURE_NAME_PREFIX = "dark:";
+	
+	
+}
