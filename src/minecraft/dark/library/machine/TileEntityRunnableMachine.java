14d3054e5b38ccf81444ef3770a55b8ae34ebf73
Robert Seifert <rseifert.phone@gmail.com>
2013-06-14 12:32:21 -0400

    Changed runnableMachine to not rely on Calc's code
    Sorry @Calclavia if this looks like copying but i want to use this class
    in all my mods without including your library. This way i can allow
    other modders to work on my code without asking for your code.

--- src/minecraft/dark/library/machine/TileEntityRunnableMachine.java
+++ src/minecraft/dark/library/machine/TileEntityRunnableMachine.java
@@ -1,27 +1,60 @@
 package dark.library.machine;
 
+import ic2.api.energy.event.EnergyTileLoadEvent;
+import ic2.api.energy.event.EnergyTileUnloadEvent;
+import ic2.api.energy.tile.IEnergySink;
+import ic2.api.Direction;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeDirection;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.oredict.OreDictionary;
-import calclavia.lib.TileEntityUniversalRunnable;
+import universalelectricity.core.UniversalElectricity;
+import universalelectricity.core.block.IConnector;
+import universalelectricity.core.block.IVoltage;
+import universalelectricity.core.electricity.ElectricityPack;
+import universalelectricity.prefab.implement.IRotatable;
+import universalelectricity.prefab.tile.TileEntityElectricityRunnable;
+import buildcraft.api.power.IPowerProvider;
+import buildcraft.api.power.IPowerReceptor;
+import buildcraft.api.power.PowerFramework;
 import dark.core.power.PowerSystems;
 
-public class TileEntityRunnableMachine extends TileEntityUniversalRunnable
+public class TileEntityRunnableMachine extends TileEntityElectricityRunnable implements IPowerReceptor, IEnergySink, IConnector, IVoltage
 {
 	public static String powerToggleItemID = "battery";
-	
+
 	protected boolean runPowerless = false;
 
-	@Override
-	public void updateEntity()
+	private IPowerProvider powerProvider;
+
+	public TileEntityRunnableMachine()
 	{
-		/* CREATES FREE POWER IF NO POWER PROVIDER IS FOUND OR BLOCK WAS SET TO RUN WITHOUT POWER */
-		if (this.wattsReceived < this.getWattBuffer() && (this.runPowerless || PowerSystems.runPowerLess(PowerSystems.INDUSTRIALCRAFT, PowerSystems.BUILDCRAFT, PowerSystems.MEKANISM)))
+		if (PowerFramework.currentFramework != null)
 		{
-			this.wattsReceived += Math.max(this.getWattBuffer() - this.wattsReceived, 0);
+			if (this.powerProvider == null)
+			{
+				this.powerProvider = PowerFramework.currentFramework.createPowerProvider();
+				this.powerProvider.configure(0, 0, Integer.MAX_VALUE, 0, Integer.MAX_VALUE);
+			}
 		}
-		
-		super.updateEntity();
+	}
+
+	@Override
+	public void initiate()
+	{
+		super.initiate();
+		MinecraftForge.EVENT_BUS.post(new EnergyTileLoadEvent(this));
+	}
+
+	@Override
+	public void invalidate()
+	{
+		MinecraftForge.EVENT_BUS.post(new EnergyTileUnloadEvent(this));
+		super.invalidate();
 	}
 
 	@Override
@@ -53,5 +86,126 @@ public class TileEntityRunnableMachine extends TileEntityUniversalRunnable
 				break;
 			}
 		}
+	}
+
+	@Override
+	public void updateEntity()
+	{
+		super.updateEntity();
+
+		if (this.wattsReceived < this.getWattBuffer() && (this.runPowerless || PowerSystems.runPowerLess(PowerSystems.INDUSTRIALCRAFT, PowerSystems.BUILDCRAFT, PowerSystems.MEKANISM)))
+		{
+			this.wattsReceived += Math.max(this.getWattBuffer() - this.wattsReceived, 0);
+		}
+
+		if (this.powerProvider != null)
+		{
+			int requiredEnergy = (int) (this.getRequest().getWatts() * UniversalElectricity.TO_BC_RATIO);
+			float energyReceived = this.powerProvider.useEnergy(requiredEnergy, requiredEnergy, true);
+			this.onReceive(ElectricityPack.getFromWatts(UniversalElectricity.BC3_RATIO * energyReceived, this.getVoltage()));
+		}
+	}
+
+	@Override
+	public boolean canConnect(ForgeDirection direction)
+	{
+		if (this instanceof IRotatable)
+		{
+			return direction == ForgeDirection.getOrientation(this.getBlockMetadata()).getOpposite();
+		}
+
+		return true;
+	}
+
+	public ForgeDirection getDirection(IBlockAccess world, int x, int y, int z)
+	{
+		return ForgeDirection.getOrientation(this.getBlockMetadata());
+	}
+
+	public void setDirection(World world, int x, int y, int z, ForgeDirection facingDirection)
+	{
+		this.worldObj.setBlockMetadataWithNotify(this.xCoord, this.yCoord, this.zCoord, facingDirection.ordinal(), 2);
+	}
+
+	/**
+	 * IC2
+	 */
+	@Override
+	public boolean acceptsEnergyFrom(TileEntity emitter, Direction direction)
+	{
+		if (this.getConsumingSides() != null)
+		{
+			return this.getConsumingSides().contains(direction.toForgeDirection());
+		}
+		else
+		{
+			return true;
+		}
+	}
+
+	@Override
+	public boolean isAddedToEnergyNet()
+	{
+		return this.ticks > 0;
+	}
+
+	@Override
+	public int demandsEnergy()
+	{
+		return (int) Math.ceil(this.getRequest().getWatts() * UniversalElectricity.TO_IC2_RATIO);
+	}
+
+	@Override
+	public int injectEnergy(Direction direction, int i)
+	{
+		double givenElectricity = i * UniversalElectricity.IC2_RATIO;
+		double rejects = 0;
+
+		if (givenElectricity > this.getWattBuffer())
+		{
+			rejects = givenElectricity - this.getRequest().getWatts();
+		}
+
+		this.onReceive(new ElectricityPack(givenElectricity / this.getVoltage(), this.getVoltage()));
+
+		return (int) (rejects * UniversalElectricity.TO_IC2_RATIO);
+	}
+
+	@Override
+	public int getMaxSafeInput()
+	{
+		return 2048;
+	}
+
+	/**
+	 * Buildcraft
+	 */
+	@Override
+	public void setPowerProvider(IPowerProvider provider)
+	{
+		this.powerProvider = provider;
+	}
+
+	@Override
+	public IPowerProvider getPowerProvider()
+	{
+		return this.powerProvider;
+	}
+
+	@Override
+	public void doWork()
+	{
+
+	}
+
+	@Override
+	public int powerRequest(ForgeDirection from)
+	{
+		if (this.canConnect(from))
+		{
+			return (int) (this.getRequest().getWatts() * UniversalElectricity.TO_BC_RATIO);
+		}
+
+		return 0;
 	}
 }
