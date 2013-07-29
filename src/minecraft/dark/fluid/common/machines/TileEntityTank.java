604703ab285ba437be8d0168140b359abcd5fb05
DarkGuardsman <rseifert.phone@gmail.com>
2013-07-29 00:51:57 -0400

    Fixed nbt writing crash

--- src/minecraft/dark/fluid/common/machines/TileEntityTank.java
+++ src/minecraft/dark/fluid/common/machines/TileEntityTank.java
@@ -116,7 +116,7 @@ public class TileEntityTank extends TileEntityFluidStorage implements IFluidHand
     public Packet getDescriptionPacket()
     {
         FluidStack stack = new FluidStack(FluidRegistry.WATER, 0);
-        if (this.getTank().getFluid() != null)
+        if (this.getTank().getFluid() != null && this.getTank().getFluid().getFluid() != null)
         {
             stack = this.getTank().getFluid();
         }
@@ -194,7 +194,7 @@ public class TileEntityTank extends TileEntityFluidStorage implements IFluidHand
     }
 
     /** Checks to make sure the connection is valid to the tileEntity
-     * 
+     *
      * @param tileEntity - the tileEntity being checked
      * @param side - side the connection is too */
     public void validateConnectionSide(TileEntity tileEntity, ForgeDirection side)
