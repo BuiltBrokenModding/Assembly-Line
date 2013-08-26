842b0f69d51dd3e76688ffe231e1ed2b4c2261a7
DarkGuardsman <rseifert.phone@gmail.com>
2013-08-26 14:36:25 -0400

    Temp using BC wire render
    I plan to make a redpower like wire render later but first we need to
    make it work. The render is just for visual aid rather than anything
    else.

--- src/dark/common/transmit/BlockWire.java
+++ src/dark/common/transmit/BlockWire.java
@@ -1,10 +1,13 @@
 package dark.common.transmit;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import net.minecraftforge.common.Configuration;
+import dark.core.DarkMain;
 import dark.core.blocks.BlockMachine;
 
 public class BlockWire extends BlockMachine
@@ -13,8 +16,36 @@ public class BlockWire extends BlockMachine
     public BlockWire(Configuration config, int blockID)
     {
         super("DMWire", config, blockID, Material.cloth);
+        this.setStepSound(soundClothFootstep);
+        this.setResistance(0.2F);
+        this.setHardness(0.1f);
+        this.setBlockBounds(0.3f, 0.3f, 0.3f, 0.7f, 0.7f, 0.7f);
         this.setCreativeTab(CreativeTabs.tabRedstone);
-        this.setBlockBounds(0, 0, 0, 1, .3f, 1);
+        Block.setBurnProperties(this.blockID, 30, 60);
+    }
+
+    @Override
+    public void registerIcons(IconRegister par1IconRegister)
+    {
+        this.blockIcon = par1IconRegister.registerIcon(DarkMain.getInstance().PREFIX +"CopperWire");
+    }
+
+    @Override
+    public boolean isOpaqueCube()
+    {
+        return false;
+    }
+
+    @Override
+    public boolean renderAsNormalBlock()
+    {
+        return false;
+    }
+
+    @Override
+    public int getRenderType()
+    {
+        return -1;
     }
 
     @Override
