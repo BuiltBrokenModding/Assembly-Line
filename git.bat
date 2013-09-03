a0d4b364c508341d8c01e8515a89ddffa99a0b49
DarkGuardsman <rseifert.phone@gmail.com>
2013-09-03 01:54:41 -0400

    Created basic build file
    May need some more work so if commits start to pop up with",,," you'll
    know why.

---
+++ git.bat
@@ -0,0 +1,4 @@
+git clone https://github.com/DarkGuardsman/Fluid-Mechanics.git
+git clone https://github.com/DarkGuardsman/Assembly-Line.git
+git clone https://github.com/DarkGuardsman/FarmTech.git
+git clone https://github.com/DarkGuardsman/Greater-Security.git
