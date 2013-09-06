298f5ad91b9b85545e0f318af374092ff1e4b624
DarkGuardsman <rseifert.phone@gmail.com>
2013-09-05 20:45:55 -0400

    @SideOnly(Side.CLIENT)

--- src/dark/farmtech/client/ClientProxy.java
+++ src/dark/farmtech/client/ClientProxy.java
@@ -1,7 +1,9 @@
 package dark.farmtech.client;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import dark.farmtech.CommonProxy;
-
+@SideOnly(Side.CLIENT)
 public class ClientProxy extends CommonProxy
 {
 
--- src/dark/farmtech/client/GuiCompostBox.java
+++ src/dark/farmtech/client/GuiCompostBox.java
@@ -1,5 +1,9 @@
 package dark.farmtech.client;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+@SideOnly(Side.CLIENT)
 public class GuiCompostBox
 {
 
