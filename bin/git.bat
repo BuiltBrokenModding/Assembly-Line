3c5540004465c184f26d69f7fe2731bffd1b7347
DarkGuardsman <rseifert.phone@gmail.com>
2013-09-06 20:47:40 -0400

    redirected git clone

--- bin/git.bat
+++ bin/git.bat
@@ -1,4 +1,9 @@
+cd ..
+mkdir downloads
+cd downloads
 git clone https://github.com/DarkGuardsman/Fluid-Mechanics.git
+
 git clone https://github.com/DarkGuardsman/Assembly-Line.git
+
 git clone https://github.com/DarkGuardsman/FarmTech.git
 git clone https://github.com/DarkGuardsman/Greater-Security.git
--- bin/git.sh
+++ bin/git.sh
@@ -1,3 +1,6 @@
+cd ..
+mkdir downloads
+cd downloads
 git clone https://github.com/DarkGuardsman/Fluid-Mechanics.git
 git clone https://github.com/DarkGuardsman/Assembly-Line.git
 git clone https://github.com/DarkGuardsman/FarmTech.git
