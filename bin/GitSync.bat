78208ed1ac6de585684d0a03ab76bb1dd6e2993a
DarkGuardsman <rseifert.phone@gmail.com>
2013-09-16 07:34:18 -0400

    Auto-Sync

--- bin/GitSync.bat
+++ bin/GitSync.bat
@@ -6,24 +6,24 @@ IF NOT "a%1"=="a" (set "com=%1")
 IF NOT "a%2"=="a" (set "msg=%2")
 cd ..\
 git commit -am "%com%%msg%"
-git push
+git push origin master
 cd ..\
 cd Assembly-Line
 git commit -am "%com%%msg%"
-git push
+git push origin master
 cd ..\
 cd FarmTech
 git commit -am "%com%%msg%"
-git push
+git push origin master
 cd ..\
 cd Fluid-Mechanics
 git commit -am "%com%%msg%"
-git push
+git push origin master
 cd ..\
 cd GreaterProtection
 git commit -am "%com%%msg%"
-git push
+git push origin master
 cd ..\
 cd GSM
 git commit -am "%com%%msg%"
-git push
+git push origin master
