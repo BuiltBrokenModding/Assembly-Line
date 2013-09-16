1cae57399d3792eda2210df90efaae401b876d09
DarkGuardsman <rseifert.phone@gmail.com>
2013-09-16 13:35:19 -0400

    Think i got the gitChangeLog.bat working
    Unsure as i need to do more testing.

--- bin/GitChangeLog.bat
+++ bin/GitChangeLog.bat
@@ -1,2 +1,6 @@
 cd ..
-git log --pretty="%s"  >> changeLog.txt
+IF EXIST LastBuildData.txt (
+	git log --pretty="%s" --since={LastBuildData.txt}  >> changeLog.txt
+)
+echo %date:~10,4%-%date:~4,2%-%date:~7,2% > LastBuildData.txt
+PAUSE
