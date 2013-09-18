0896387e94bea62f6f381654a031e62ee963de9c
DarkGuardsman <rseifert.phone@gmail.com>
2013-09-18 06:56:27 -0400

    Changes

--- bin/GitSync.bat
+++ bin/GitSync.bat
@@ -1,29 +1,27 @@
 rem set defaults:
-set "com=Auto"
-set "msg=-Sync"
+set "com=Auto-Sync"
 rem set parameters:
 IF NOT "a%1"=="a" (set "com=%1")
-IF NOT "a%2"=="a" (set "msg=%2")
 cd ..\
-git commit -am "%com%%msg%"
+git commit -am "%com%"
 git push origin master
 cd ..\
 cd Assembly-Line
-git commit -am "%com%%msg%"
+git commit -am "%com%"
 git push origin master
 cd ..\
 cd FarmTech
-git commit -am "%com%%msg%"
+git commit -am "%com%"
 git push origin master
 cd ..\
 cd Fluid-Mechanics
-git commit -am "%com%%msg%"
+git commit -am "%com%"
 git push origin master
 cd ..\
 cd GreaterProtection
-git commit -am "%com%%msg%"
+git commit -am "%com%"
 git push origin master
 cd ..\
 cd GSM
-git commit -am "%com%%msg%"
+git commit -am "%com%"
 git push origin master
