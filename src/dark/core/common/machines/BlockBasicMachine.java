be1f8a4efb1569d83be22f3e0c8071026e1bbaff
DarkGuardsman <rseifert.phone@gmail.com>
2013-09-18 09:22:41 -0400

    Started working on some basic machines
    based on Old Basic Component machines. I do plan to rework every aspect
    but am trying to maintain that same fill as the old machines. This is
    why some of the code is copied so that the handling, and slot IDs match
    up.

---
+++ src/dark/core/common/machines/BlockBasicMachine.java
@@ -0,0 +1,217 @@
+package dark.core.common.machines;
+
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.world.World;
+import net.minecraftforge.common.Configuration;
+import universalelectricity.core.UniversalElectricity;
+import dark.core.common.CommonProxy;
+import dark.core.common.DarkMain;
+import dark.core.prefab.IExtraObjectInfo;
+import dark.core.prefab.helpers.Pair;
+import dark.core.prefab.machine.BlockMachine;
+
+public class BlockBasicMachine extends BlockMachine implements IExtraObjectInfo
+{
+
+    public BlockBasicMachine(int id)
+    {
+        super("BasicMachine", DarkMain.CONFIGURATION, id, UniversalElectricity.machine);
+        this.setCreativeTab(CreativeTabs.tabDecorations);
+        this.setStepSound(soundMetalFootstep);
+    }
+
+    @Override
+    public void randomDisplayTick(World par1World, int x, int y, int z, Random par5Random)
+    {
+        TileEntity tile = par1World.getBlockTileEntity(x, y, z);
+
+        if (tile instanceof TileEntityCoalGenerator)
+        {
+            TileEntityCoalGenerator tileEntity = (TileEntityCoalGenerator) tile;
+            if (tileEntity.generateWatts > 0)
+            {
+                int metadata = par1World.getBlockMetadata(x, y, z);
+                float var7 = x + 0.5F;
+                float var8 = y + 0.0F + par5Random.nextFloat() * 6.0F / 16.0F;
+                float var9 = z + 0.5F;
+                float var10 = 0.52F;
+                float var11 = par5Random.nextFloat() * 0.6F - 0.3F;
+
+                if (metadata == 3)
+                {
+                    par1World.spawnParticle("smoke", var7 - var10, var8, var9 + var11, 0.0D, 0.0D, 0.0D);
+                    par1World.spawnParticle("flame", var7 - var10, var8, var9 + var11, 0.0D, 0.0D, 0.0D);
+                }
+                else if (metadata == 2)
+                {
+                    par1World.spawnParticle("smoke", var7 + var10, var8, var9 + var11, 0.0D, 0.0D, 0.0D);
+                    par1World.spawnParticle("flame", var7 + var10, var8, var9 + var11, 0.0D, 0.0D, 0.0D);
+                }
+                else if (metadata == 1)
+                {
+                    par1World.spawnParticle("smoke", var7 + var11, var8, var9 - var10, 0.0D, 0.0D, 0.0D);
+                    par1World.spawnParticle("flame", var7 + var11, var8, var9 - var10, 0.0D, 0.0D, 0.0D);
+                }
+                else if (metadata == 0)
+                {
+                    par1World.spawnParticle("smoke", var7 + var11, var8, var9 + var10, 0.0D, 0.0D, 0.0D);
+                    par1World.spawnParticle("flame", var7 + var11, var8, var9 + var10, 0.0D, 0.0D, 0.0D);
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean onUseWrench(World world, int x, int y, int z, EntityPlayer entityPlayer, int side, float hitX, float hitY, float hitZ)
+    {
+        if (world.getBlockMetadata(x, y, z) % 4 < 3)
+        {
+            world.setBlockMetadataWithNotify(x, y, z, world.getBlockMetadata(x, y, z) + 1, 3);
+            return true;
+        }
+        else
+        {
+            world.setBlockMetadataWithNotify(x, y, z, world.getBlockMetadata(x, y, z) - 3, 3);
+            return true;
+        }
+    }
+
+    @Override
+    public void getTileEntities(int blockID, Set<Pair<String, Class<? extends TileEntity>>> list)
+    {
+        for (BasicMachineData data : BasicMachineData.values())
+        {
+            list.add(new Pair<String, Class<? extends TileEntity>>("DC" + data.unlocalizedName, data.clazz));
+        }
+    }
+
+    @Override
+    public boolean hasExtraConfigs()
+    {
+        return false;
+    }
+
+    @Override
+    public void loadExtraConfigs(Configuration config)
+    {
+    }
+
+    @Override
+    public void loadRecipes()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void loadOreNames()
+    {
+    }
+
+    /** Called when the block is right clicked by the player */
+    @Override
+    public boolean onMachineActivated(World par1World, int x, int y, int z, EntityPlayer par5EntityPlayer, int side, float hitX, float hitY, float hitZ)
+    {
+
+        if (!par1World.isRemote)
+        {
+            par5EntityPlayer.openGui(DarkMain.getInstance(), BasicMachineData.values()[par1World.getBlockMetadata(x, y, z) / 4].guiID, par1World, x, y, z);
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean isOpaqueCube()
+    {
+        return false;
+    }
+
+    @Override
+    public boolean renderAsNormalBlock()
+    {
+        return false;
+    }
+
+    @Override
+    public TileEntity createTileEntity(World world, int metadata)
+    {
+        try
+        {
+            return BasicMachineData.values()[metadata / 4].clazz.newInstance();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+
+    @Override
+    public void getSubBlocks(int par1, CreativeTabs par2CreativeTabs, List par3List)
+    {
+        par3List.add(new ItemStack(par1, 1, 0));
+        par3List.add(new ItemStack(par1, 1, 8));
+        par3List.add(new ItemStack(par1, 1, 12));
+    }
+
+    @Override
+    public int damageDropped(int metadata)
+    {
+        return metadata / 4;
+    }
+
+    @Override
+    public ItemStack getPickBlock(MovingObjectPosition target, World world, int x, int y, int z)
+    {
+        int id = idPicked(world, x, y, z);
+
+        if (id == 0)
+        {
+            return null;
+        }
+
+        Item item = Item.itemsList[id];
+
+        if (item == null)
+        {
+            return null;
+        }
+
+        int metadata = getDamageValue(world, x, y, z);
+
+        return new ItemStack(id, 1, metadata);
+    }
+
+    public static enum BasicMachineData
+    {
+        GENERATOR_COAL("coalgen", 0, CommonProxy.GUI_COAL_GEN, TileEntityCoalGenerator.class),
+        GENERATOR_FUEL("fuelgen", 4, CommonProxy.GUI_FUEL_GEN, TileEntityCoalGenerator.class),
+        BATTERY_BOX("batterybox", 8, CommonProxy.GUI_BATTERY_BOX, TileEntityBatteryBox.class),
+        ELECTRIC_FURNACE("electricfurnace", 12, CommonProxy.GUI_FURNACE_ELEC, TileEntityElectricFurnace.class);
+
+        public String unlocalizedName;
+        public int startMeta, guiID;
+        public boolean enabled = true;
+        public boolean allowCrafting = true;
+        public Class<? extends TileEntity> clazz;
+
+        private BasicMachineData(String name, int meta, int guiID, Class<? extends TileEntity> clazz)
+        {
+            this.unlocalizedName = name;
+            this.startMeta = meta;
+            this.guiID = guiID;
+            this.clazz = clazz;
+        }
+    }
+}
---
+++ src/dark/core/common/machines/TileEntityCoalGenerator.java
@@ -0,0 +1,219 @@
+package dark.core.common.machines;
+
+import java.util.EnumSet;
+import java.util.HashSet;
+import java.util.Set;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.INetworkManager;
+import net.minecraft.network.packet.Packet;
+import net.minecraft.network.packet.Packet250CustomPayload;
+import net.minecraftforge.common.ForgeDirection;
+import universalelectricity.core.electricity.ElectricityPack;
+
+import com.google.common.io.ByteArrayDataInput;
+
+import cpw.mods.fml.common.network.PacketDispatcher;
+import cpw.mods.fml.common.network.Player;
+import cpw.mods.fml.common.registry.LanguageRegistry;
+import dark.core.network.PacketHandler;
+import dark.core.prefab.machine.TileEntityEnergyMachine;
+
+public class TileEntityCoalGenerator extends TileEntityEnergyMachine
+{
+    /** Maximum amount of energy needed to generate electricity */
+    public static final float MAX_GENERATE_WATTS = 0.5f;
+
+    /** Amount of heat the coal generator needs before generating electricity. */
+
+    public static final float MIN_GENERATE_WATTS = MAX_GENERATE_WATTS * 0.1f;
+
+    private static final float BASE_ACCELERATION = 0.000001f;
+
+    /** Per second */
+    public float prevGenerateWatts, generateWatts = 0;
+
+    /** The number of ticks that a fresh copy of the currently-burning item would keep the furnace
+     * burning for */
+    public int itemCookTime = 0;
+
+    public final Set<EntityPlayer> playersUsing = new HashSet<EntityPlayer>();
+
+    @Override
+    public void updateEntity()
+    {
+        this.setEnergyStored(this.generateWatts);
+
+        super.updateEntity();
+
+        if (!this.worldObj.isRemote)
+        {
+            this.prevGenerateWatts = this.generateWatts;
+
+            if (this.itemCookTime > 0)
+            {
+                this.itemCookTime--;
+
+                if (this.getEnergyStored() < this.getMaxEnergyStored())
+                {
+                    this.generateWatts = Math.min(this.generateWatts + Math.min((this.generateWatts * 0.007F + BASE_ACCELERATION), 0.007F), TileEntityCoalGenerator.MAX_GENERATE_WATTS);
+                }
+            }
+
+            if (this.getInventory().getStackInSlot(0) != null && this.getEnergyStored() < this.getMaxEnergyStored())
+            {
+                if (this.getInventory().getStackInSlot(0).getItem().itemID == Item.coal.itemID)
+                {
+                    if (this.itemCookTime <= 0)
+                    {
+                        this.itemCookTime = 320;
+                        this.decrStackSize(0, 1);
+                    }
+                }
+            }
+
+            if (this.itemCookTime <= 0)
+            {
+                this.generateWatts = Math.max(this.generateWatts - 0.008F, 0);
+            }
+
+            if (this.ticks % 3 == 0)
+            {
+                for (EntityPlayer player : this.playersUsing)
+                {
+                    PacketDispatcher.sendPacketToPlayer(getDescriptionPacket(), (Player) player);
+                }
+            }
+
+            if (this.prevGenerateWatts <= 0 && this.generateWatts > 0 || this.prevGenerateWatts > 0 && this.generateWatts <= 0)
+            {
+                PacketHandler.instance().sendPacketToClients(getDescriptionPacket(), this.worldObj);
+            }
+        }
+    }
+
+    @Override
+    public Packet getDescriptionPacket()
+    {
+        return PacketHandler.instance().getPacket(this.getChannel(), this, "gen", this.generateWatts, this.itemCookTime);
+    }
+
+    @Override
+    public void handlePacketData(INetworkManager network, int type, Packet250CustomPayload packet, EntityPlayer player, ByteArrayDataInput dataStream)
+    {
+        try
+        {
+            if (this.worldObj.isRemote)
+            {
+                this.generateWatts = dataStream.readFloat();
+                this.itemCookTime = dataStream.readInt();
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void openChest()
+    {
+    }
+
+    @Override
+    public void closeChest()
+    {
+    }
+
+    /** Reads a tile entity from NBT. */
+    @Override
+    public void readFromNBT(NBTTagCompound par1NBTTagCompound)
+    {
+        super.readFromNBT(par1NBTTagCompound);
+        this.itemCookTime = par1NBTTagCompound.getInteger("itemCookTime");
+        this.generateWatts = par1NBTTagCompound.getFloat("generateRate");
+    }
+
+    /** Writes a tile entity to NBT. */
+    @Override
+    public void writeToNBT(NBTTagCompound par1NBTTagCompound)
+    {
+        super.writeToNBT(par1NBTTagCompound);
+        par1NBTTagCompound.setInteger("itemCookTime", this.itemCookTime);
+        par1NBTTagCompound.setFloat("generateRate", this.generateWatts);
+    }
+
+    @Override
+    public String getInvName()
+    {
+        return LanguageRegistry.instance().getStringLocalization("gui.coalgen.name");
+    }
+
+    @Override
+    public boolean isItemValidForSlot(int slotID, ItemStack itemstack)
+    {
+        return itemstack.itemID == Item.coal.itemID;
+    }
+
+    @Override
+    public int[] getAccessibleSlotsFromSide(int var1)
+    {
+        return new int[] { 0 };
+    }
+
+    @Override
+    public boolean canInsertItem(int slotID, ItemStack itemstack, int j)
+    {
+        return this.isItemValidForSlot(slotID, itemstack);
+    }
+
+    @Override
+    public boolean canExtractItem(int slotID, ItemStack itemstack, int j)
+    {
+        return slotID == 0;
+    }
+
+    @Override
+    public float receiveElectricity(ForgeDirection from, ElectricityPack electricityPack, boolean doReceive)
+    {
+        return 0;
+    }
+
+    @Override
+    public float getRequest(ForgeDirection direction)
+    {
+        return 0;
+    }
+
+    @Override
+    public float getProvide(ForgeDirection direction)
+    {
+        if (direction == ForgeDirection.getOrientation(this.getBlockMetadata() + 2))
+        {
+            return this.generateWatts < TileEntityCoalGenerator.MIN_GENERATE_WATTS ? 0 : this.generateWatts;
+        }
+
+        return 0.0F;
+    }
+
+    @Override
+    public EnumSet<ForgeDirection> getInputDirections()
+    {
+        return EnumSet.noneOf(ForgeDirection.class);
+    }
+
+    @Override
+    public EnumSet<ForgeDirection> getOutputDirections()
+    {
+        return EnumSet.of(ForgeDirection.getOrientation(this.getBlockMetadata() + 2));
+    }
+
+    @Override
+    public float getMaxEnergyStored()
+    {
+        return MAX_GENERATE_WATTS;
+    }
+}
