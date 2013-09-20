99f8af9d5ca8b899bacd4849576dd220fe5701f4
DarkGuardsman <rseifert.phone@gmail.com>
2013-09-20 13:58:07 -0400

    abandoned annotion use on blockSolarPanel
    tried to use the solar panel for a test of the annotion system for
    registering tileEntities and loading block based configs. However, my
    code could no get the annotions for use.  I will have to correct it and
    retest it on another block.

--- src/dark/core/common/machines/BlockSolarPanel.java
+++ src/dark/core/common/machines/BlockSolarPanel.java
@@ -1,18 +1,19 @@
 package dark.core.common.machines;
 
+import java.util.Set;
+
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import net.minecraftforge.common.Configuration;
 import universalelectricity.core.UniversalElectricity;
 import dark.core.client.renders.BlockRenderingHandler;
 import dark.core.common.DMCreativeTab;
+import dark.core.prefab.IExtraObjectInfo;
+import dark.core.prefab.helpers.Pair;
 import dark.core.prefab.machine.BlockMachine;
-import dark.core.registration.BlockConfigFile;
 import dark.core.registration.ModObjectRegistry.BlockBuildData;
-import dark.core.registration.BlockTileEntityInfo;
 
-@BlockTileEntityInfo(tileEntities = { TileEntitySolarPanel.class }, tileEntitiesNames = { "DMSolarCell" })
-public class BlockSolarPanel extends BlockMachine
+public class BlockSolarPanel extends BlockMachine implements IExtraObjectInfo
 {
     public static float tickRate = 10;
     public static float wattPerLightValue = .012f;
@@ -22,13 +23,6 @@ public class BlockSolarPanel extends BlockMachine
         super(new BlockBuildData(BlockSolarPanel.class, "BlockSolarPanel", UniversalElectricity.machine));
         this.setBlockBounds(0, 0, 0, 1f, .3f, 1f);
         this.setCreativeTab(DMCreativeTab.tabIndustrial);
-    }
-
-    @BlockConfigFile
-    public void loadBlockConfig(Configuration config)
-    {
-        tickRate = config.get("settings", "PanelUpdateRate", tickRate).getInt();
-        wattPerLightValue = (config.get("settings", "WattPerLightvalue", (int) wattPerLightValue * 1000, "Value * 15 equals full output").getInt() / 1000);
     }
 
     public TileEntity createNewTileEntity(World world)
@@ -52,5 +46,39 @@ public class BlockSolarPanel extends BlockMachine
     public int getRenderType()
     {
         return BlockRenderingHandler.BLOCK_RENDER_ID;
+    }
+
+    @Override
+    public void getTileEntities(int blockID, Set<Pair<String, Class<? extends TileEntity>>> list)
+    {
+       list.add(new Pair<String,Class<? extends TileEntity>>("DMSolarCell", TileEntitySolarPanel.class));
+
+    }
+
+    @Override
+    public boolean hasExtraConfigs()
+    {
+        return true;
+    }
+
+    @Override
+    public void loadExtraConfigs(Configuration config)
+    {
+        tickRate = config.get("settings", "PanelUpdateRate", tickRate).getInt();
+        wattPerLightValue = (config.get("settings", "WattPerLightvalue", (int) wattPerLightValue * 1000, "Value * 15 equals full output").getInt() / 1000);
+    }
+
+    @Override
+    public void loadRecipes()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void loadOreNames()
+    {
+        // TODO Auto-generated method stub
+
     }
 }
