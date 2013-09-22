0c0f08c736932d53377df1fec80d1ca344e66f67
DarkGuardsman <rseifert.phone@gmail.com>
2013-09-22 00:05:33 -0400

    ReworkedExtraInfoLoading

--- src/dark/fluid/common/BlockFM.java
+++ src/dark/fluid/common/BlockFM.java
@@ -1,39 +1,15 @@
 package dark.fluid.common;
 
 import net.minecraft.block.material.Material;
-import net.minecraftforge.common.Configuration;
 import dark.core.common.DMCreativeTab;
-import dark.core.prefab.IExtraObjectInfo;
 import dark.core.prefab.machine.BlockMachine;
 import dark.core.registration.ModObjectRegistry.BlockBuildData;
 
-public abstract class BlockFM extends BlockMachine implements IExtraObjectInfo
+public abstract class BlockFM extends BlockMachine
 {
 
     public BlockFM(Class<? extends BlockFM> blockClass, String name, Material material)
     {
         super(new BlockBuildData(blockClass, name, material).setConfigProvider(FluidMech.CONFIGURATION).setCreativeTab(DMCreativeTab.tabHydrualic));
     }
-
-    @Override
-    public boolean hasExtraConfigs()
-    {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    @Override
-    public void loadExtraConfigs(Configuration config)
-    {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void loadOreNames()
-    {
-        // TODO Auto-generated method stub
-
-    }
-
 }
