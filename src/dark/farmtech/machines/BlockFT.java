75b317d83a710243a8fde8c4fe8e39989f8df31e
DarkGuardsman <rseifert.phone@gmail.com>
2013-09-22 00:05:30 -0400

    ReworkedExtraInfoLoading

--- src/dark/farmtech/machines/BlockFT.java
+++ src/dark/farmtech/machines/BlockFT.java
@@ -1,12 +1,6 @@
 package dark.farmtech.machines;
 
-import java.util.Set;
-
 import net.minecraft.block.material.Material;
-import net.minecraft.tileentity.TileEntity;
-import net.minecraftforge.common.Configuration;
-import dark.core.prefab.IExtraObjectInfo;
-import dark.core.prefab.helpers.Pair;
 import dark.core.prefab.machine.BlockMachine;
 import dark.core.registration.ModObjectRegistry.BlockBuildData;
 import dark.farmtech.FarmTech;
@@ -14,37 +8,12 @@ import dark.farmtech.FarmTech;
 /** Prefab class for all farm blocks to remove the need for some configuration of the super class
  *
  * @author Darkguardsman */
-public abstract class BlockFT extends BlockMachine implements IExtraObjectInfo
+public abstract class BlockFT extends BlockMachine
 {
-    private boolean hasConfigFile = false;
 
     public BlockFT(Class<? extends BlockMachine> blockClass, String name, Material material)
     {
         super(new BlockBuildData(blockClass, name, material).setCreativeTab(FarmTech.TabFarmTech));
-    }
-
-    @Override
-    public void getTileEntities(int blockID, Set<Pair<String, Class<? extends TileEntity>>> list)
-    {
-
-    }
-
-    @Override
-    public boolean hasExtraConfigs()
-    {
-        return hasConfigFile;
-    }
-
-    @Override
-    public void loadExtraConfigs(Configuration config)
-    {
-
-    }
-
-    @Override
-    public void loadOreNames()
-    {
-
     }
 
 }
