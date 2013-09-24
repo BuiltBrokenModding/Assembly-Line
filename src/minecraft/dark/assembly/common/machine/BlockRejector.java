6eb7a36c4e4a1b767c5057abaf8bbeb38994b18b
DarkGuardsman <rseifert.phone@gmail.com>
2013-09-24 11:24:43 -0400

    Created alt block texture in place of rejector render
    Since the renderers can be disabled i need to slowly work my way threw
    and provide block based looks for all non-active models. Active means
    things like armbot and sentry guns were the render has a function to
    play with how it works.

--- src/minecraft/dark/assembly/common/machine/BlockRejector.java
+++ src/minecraft/dark/assembly/common/machine/BlockRejector.java
@@ -1,16 +1,22 @@
 package dark.assembly.common.machine;
 
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Icon;
 import net.minecraft.world.World;
 import universalelectricity.core.UniversalElectricity;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import dark.assembly.client.render.BlockRenderingHandler;
+import dark.assembly.common.AssemblyLine;
 import dark.assembly.common.imprinter.prefab.BlockImprintable;
 import dark.core.registration.ModObjectRegistry.BlockBuildData;
 
 public class BlockRejector extends BlockImprintable
 {
+    @SideOnly(Side.CLIENT)
+    protected Icon front;
+
     public BlockRejector()
     {
         super(new BlockBuildData(BlockRejector.class, "rejector", UniversalElectricity.machine));
@@ -22,24 +28,30 @@ public class BlockRejector extends BlockImprintable
         return new TileEntityRejector();
     }
 
-    @Override
-    public boolean isOpaqueCube()
-    {
-        return false;
-    }
-
-    @Override
-    @SideOnly(Side.CLIENT)
-    public boolean renderAsNormalBlock()
-    {
-        return false;
-    }
-
     @SideOnly(Side.CLIENT)
     @Override
     public int getRenderType()
     {
-        return BlockRenderingHandler.BLOCK_RENDER_ID;
+        return this.zeroRendering ? 0 : BlockRenderingHandler.BLOCK_RENDER_ID;
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void registerIcons(IconRegister par1IconRegister)
+    {
+        this.blockIcon = par1IconRegister.registerIcon(AssemblyLine.instance.PREFIX + "imprinter_bottom");
+        this.front = par1IconRegister.registerIcon(AssemblyLine.instance.PREFIX + "disk_tray");
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public Icon getIcon(int side, int metadata)
+    {
+        if (side == metadata)
+        {
+            return this.front;
+        }
+        return this.blockIcon;
     }
 
 }
