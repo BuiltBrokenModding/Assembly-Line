2054562bbcbf4b237757bb11d3dc5efb82fcf37f
DarkGuardsman <rseifert.phone@gmail.com>
2013-10-20 18:00:39 -0400

    changed url link

--- src/dark/farmtech/FarmTech.java
+++ src/dark/farmtech/FarmTech.java
@@ -116,7 +116,7 @@ public class FarmTech extends ModPrefab
         meta.modId = MOD_ID;
         meta.name = MOD_NAME;
         meta.description = "Farming addon for Darks Core Machine";
-        meta.url = "www.BuiltBroken.com";
+        meta.url = "http://www.http://universalelectricity.com/coremachine";
 
         meta.logoFile = TEXTURE_DIRECTORY + "GP_Banner.png";
         meta.version = DarkMain.VERSION;
