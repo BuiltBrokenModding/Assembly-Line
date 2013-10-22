dcf0627d51549ee015a33b0b6fb781b51909a1ba
DarkGuardsman <rseifert.phone@gmail.com>
2013-10-22 16:57:34 -0400

    Updated scripts

--- bin/GitClone.bat
+++ bin/GitClone.bat
@@ -1,9 +1,8 @@
 cd ..
 mkdir downloads
 cd downloads
-git clone https://github.com/DarkGuardsman/Fluid-Mechanics.git
-
-git clone https://github.com/DarkGuardsman/Assembly-Line.git
-
-git clone https://github.com/DarkGuardsman/FarmTech.git
-git clone https://github.com/DarkGuardsman/Greater-Security.git
+git clone https://github.com/DarksCoreMachine/Fluid-Mechanics.git
+git clone https://github.com/DarksCoreMachine/Assembly-Line.git
+git clone https://github.com/DarksCoreMachine/FarmTech.git
+git clone https://github.com/DarksCoreMachine/Greater-Security.git
+git clone https://github.com/DarksCoreMachine/MechanizedMining.git
