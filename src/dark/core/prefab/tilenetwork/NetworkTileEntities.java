06c4b331b621ceac7c67a12eb79a9059cb951fe2
DarkGuardsman <rseifert.phone@gmail.com>
2013-11-08 17:40:26 -0500

    Set network to invalidate when merging

--- src/dark/core/prefab/tilenetwork/NetworkTileEntities.java
+++ src/dark/core/prefab/tilenetwork/NetworkTileEntities.java
@@ -149,17 +149,17 @@ public class NetworkTileEntities implements ITileNetwork
 
     /** Processing that needs too be done before the network merges. Use this to do final network
      * merge calculations and to cause network merge failure
-     * 
+     *
      * @param network the network that is to merge with this one
      * @param part the part at which started the network merge. Use this to cause damage if two
      * networks merge with real world style failures
-     * 
+     *
      * @return false if the merge needs to be canceled.
-     * 
+     *
      * Cases in which the network should fail to merge are were the two networks merge with error.
      * Or, in the case of pipes the two networks merge and the merge point was destroyed by
      * combination of liquids.
-     * 
+     *
      * Ex Lava and water */
     public boolean preMergeProcessing(ITileNetwork network, INetworkPart part)
     {
@@ -174,6 +174,7 @@ public class NetworkTileEntities implements ITileNetwork
         newNetwork.getMembers().addAll(this.getMembers());
         newNetwork.getMembers().addAll(network.getMembers());
         newNetwork.onCreated();
+        this.invalidate();
     }
 
     @Override
@@ -215,8 +216,6 @@ public class NetworkTileEntities implements ITileNetwork
                                     }
                                     newNetwork.onCreated();
                                 }
-                                this.cleanUpMembers();
-                                this.load();
                             }
                         }
                     }
