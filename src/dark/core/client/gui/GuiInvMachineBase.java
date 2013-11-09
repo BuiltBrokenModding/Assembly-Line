a039f46a6fcb5c50b06fde1523dbc12ed86fdc3d
Robert <rseifert.phone@gmail.com>
2013-11-09 12:43:57 -0500

    Idea reminder to create base Gui for machines

---
+++ src/dark/core/client/gui/GuiInvMachineBase.java
@@ -0,0 +1,9 @@
+package dark.core.client.gui;
+
+/** Same as the GuiMachineBase but supports inventory pages
+ * 
+ * @author DarkGuardsman */
+public class GuiInvMachineBase
+{
+
+}
---
+++ src/dark/core/client/gui/GuiMachineBase.java
@@ -0,0 +1,25 @@
+package dark.core.client.gui;
+
+import dark.core.prefab.invgui.GuiBase;
+
+/** To be used with all machine that have a gui to allow generic settings and feature all all devices
+ * 
+ * @author DarkGuardsman */
+public class GuiMachineBase extends GuiBase
+{
+
+    @Override
+    protected void drawForegroundLayer(int var2, int var3, float var1)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    protected void drawBackgroundLayer(int var2, int var3, float var1)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
