700ca95467bd6715fbfd5c66e58703603db8a9f2
DarkGuardsman <rseifert.phone@gmail.com>
2013-12-04 07:37:26 -0500

    Added built-broken-lib to the manual clone script

--- bin/GitClone.sh
+++ bin/GitClone.sh
@@ -6,4 +6,5 @@ git clone https://github.com/DarksCoreMachine/Assembly-Line.git
 git clone https://github.com/DarksCoreMachine/FarmTech.git
 git clone https://github.com/DarksCoreMachine/Greater-Security.git
 git clone https://github.com/DarksCoreMachine/MechanizedMining.git
+git clone https://github.com/DarkGuardsman/Built-Broken-Lib.git
 
