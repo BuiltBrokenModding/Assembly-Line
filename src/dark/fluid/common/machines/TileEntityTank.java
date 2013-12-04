9686422ff4a62aabd96f4563d8c02ae6938c9da1
DarkGuardsman <rseifert.phone@gmail.com>
2013-12-04 15:09:37 -0500

    Fixed an issue with tank render

--- src/dark/fluid/common/machines/TileEntityTank.java
+++ src/dark/fluid/common/machines/TileEntityTank.java
@@ -1,6 +1,12 @@
 package dark.fluid.common.machines;
 
+import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.common.ForgeDirection;
+import universalelectricity.core.vector.Vector3;
+import dark.api.fluid.INetworkFluidPart;
+import dark.api.tilenetwork.INetworkPart;
 import dark.api.tilenetwork.ITileNetwork;
+import dark.api.tilenetwork.ITileConnector.Connection;
 import dark.core.prefab.tilenetwork.fluid.NetworkFluidContainers;
 import dark.fluid.common.prefab.TileEntityFluidNetworkTile;
 
@@ -27,6 +33,22 @@ public class TileEntityTank extends TileEntityFluidNetworkTile
         if (network instanceof NetworkFluidContainers)
         {
             this.network = (NetworkFluidContainers) network;
+        }
+    }
+
+    public void validateConnectionSide(TileEntity tileEntity, ForgeDirection side)
+    {
+        if (!this.worldObj.isRemote)
+        {
+            if (tileEntity instanceof TileEntityTank)
+            {
+                if (this.canTileConnect(Connection.NETWORK, side.getOpposite()))
+                {
+                    this.getTileNetwork().mergeNetwork(((INetworkFluidPart) tileEntity).getTileNetwork(), (INetworkPart) tileEntity);
+                    this.renderConnection[side.ordinal()] = true;
+                    connectedBlocks.add(tileEntity);
+                }
+            }
         }
     }
 }
