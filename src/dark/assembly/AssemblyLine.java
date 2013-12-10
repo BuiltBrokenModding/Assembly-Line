171967322c6a45db9b62b4611962df65327f9724
DarkGuardsman <rseifert.phone@gmail.com>
2013-12-10 07:25:06 -0500

    added lang to load list

--- src/dark/assembly/AssemblyLine.java
+++ src/dark/assembly/AssemblyLine.java
@@ -91,7 +91,7 @@ public class AssemblyLine extends ModPrefab
 
     //public static final String TEXTURE_NAME_PREFIX = "assemblyline:";
 
-    private static final String[] LANGUAGES_SUPPORTED = new String[] { "en_US", "nl_NL", "fr_FR", "de_DE", "zh_CN" };
+    private static final String[] LANGUAGES_SUPPORTED = new String[] { "en_US", "nl_NL", "fr_FR", "de_DE", "zh_CN", "pt_BR" };
 
     public static final Configuration CONFIGURATION = new Configuration(new File(Loader.instance().getConfigDir(), "Dark/AssemblyLine.cfg"));
 
